0a3d32b03341f835a48eff1c7a3fd45af924a901
Todd Mohney <toddmohney@gmail.com>
2014-10-24 10:12:39 -0400

    Organize specs

---
+++ spec/integration/shared_contexts/fake_active_record_includes.rb
@@ -0,0 +1,22 @@
+shared_context "a fake active record user" do
+  with_model :FakeActiveRecordUser do
+    table do |t|
+      t.string :title
+    end
+
+    model do
+      has_many :fake_active_record_results
+    end
+  end
+end
+
+shared_context "a fake active record result" do
+  with_model :FakeActiveRecordResult do
+    table do |t|
+      t.string :title
+      t.integer :fake_active_record_user_id
+    end
+  end
+end
+
+
--- spec/integration/sub_pub_spec.rb
+++ spec/integration/sub_pub_spec.rb
@@ -1,22 +1,8 @@
 require 'spec_helper'
 
 describe SubPub do
-  with_model :FakeActiveRecordUser do
-    table do |t|
-      t.string :title
-    end
-
-    model do
-      has_many :fake_active_record_results
-    end
-  end
-
-  with_model :FakeActiveRecordResult do
-    table do |t|
-      t.string :title
-      t.integer :fake_active_record_user_id
-    end
-  end
+  include_context "a fake active record result"
+  include_context "a fake active record user"
 
   describe "initial state" do
     it "defaults enabled to true" do
@@ -135,117 +121,6 @@ describe SubPub do
     it "calls the subscriber properly" do
       CreateAccountSubscriber.subscribe_to("new_account_posted")
       SubPub::Register.instance.subscriptions.count.should == 1
-    end
-  end
-
-  describe "active record configuration" do
-    describe "after create" do
-      before do
-        class FakeActiveRecordUserSubscriber < SubPub::ActiveRecord::Subscriber
-          subscribe_to(FakeActiveRecordUser, 'after_create')
-
-          def on_publish
-            FakeActiveRecordResult.create
-          end
-        end
-      end
-
-      it "successfully calls through to the subscriber" do
-        FakeActiveRecordResult.all.size.should == 0
-        FakeActiveRecordUser.create
-        FakeActiveRecordResult.all.size.should == 1
-      end
-    end
-
-    describe "after commit" do
-      before do
-        class FakeActiveRecordUserSubscriber < SubPub::ActiveRecord::Subscriber
-          subscribe_to(FakeActiveRecordUser, 'after_commit')
-
-          def on_publish
-            FakeActiveRecordResult.create
-          end
-        end
-      end
-
-      it "successfully calls through to the subscriber" do
-        FakeActiveRecordResult.all.size.should == 0
-        FakeActiveRecordUser.create
-        FakeActiveRecordResult.all.size.should == 1
-      end
-    end
-
-    describe "after commit on create" do
-      before do
-        class FakeActiveRecordUserSubscriber < SubPub::ActiveRecord::Subscriber
-          subscribe_to(FakeActiveRecordUser, 'after_commit_on_create')
-
-          def on_publish
-            FakeActiveRecordResult.create
-          end
-        end
-      end
-
-      it "successfully calls through to the subscriber" do
-        FakeActiveRecordResult.all.size.should == 0
-        FakeActiveRecordUser.create
-        FakeActiveRecordResult.all.size.should == 1
-      end
-    end
-
-    describe "after commit on destroy" do
-      before do
-        class FakeActiveRecordUserSubscriber < SubPub::ActiveRecord::Subscriber
-          subscribe_to(FakeActiveRecordUser, 'after_commit_on_destroy')
-
-          def on_publish
-            FakeActiveRecordResult.create
-          end
-        end
-      end
-
-      it "successfully calls through to the subscriber" do
-        FakeActiveRecordResult.all.size.should == 0
-        FakeActiveRecordUser.create.destroy
-        FakeActiveRecordResult.all.size.should == 1
-      end
-    end
-
-    describe "after commit on update" do
-      before do
-        class FakeActiveRecordUserSubscriber < SubPub::ActiveRecord::Subscriber
-          subscribe_to(FakeActiveRecordUser, 'after_commit_on_update')
-
-          def on_publish
-            FakeActiveRecordResult.create
-          end
-        end
-      end
-
-      it "successfully calls through to the subscriber" do
-        FakeActiveRecordResult.all.size.should == 0
-        fake_user = FakeActiveRecordUser.create
-        fake_user.update_attributes(title: "ABC")
-        FakeActiveRecordResult.all.size.should == 1
-      end
-    end
-
-    describe "before create" do
-      before do
-        class FakeActiveRecordUserSubscriber < SubPub::ActiveRecord::Subscriber
-          subscribe_to(FakeActiveRecordUser, 'before_create')
-
-          def on_publish
-            record.fake_active_record_results.build(title: 'fooz')
-          end
-        end
-      end
-
-      it "successfully calls through to the subscriber" do
-        FakeActiveRecordResult.all.size.should == 0
-        FakeActiveRecordUser.create
-        FakeActiveRecordResult.all.size.should == 1
-      end
     end
   end
 end
